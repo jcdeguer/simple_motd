d4281f01cf45a1cd64d233c4cfbfb64b8b99af55
Juan Carlos <juan.deguer@gmail.com>
2017-03-09 13:42:31 -0300

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,40 +1,33 @@
 # == Class: simple_motd
 #
-# Full description of class simple_motd here.
+# Esta clase se escribio para modificar de forma dinamica el contenido del motd en los servidores, usando el proyecto Foreman
+# y su integracion con Puppet.
 #
 # === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
 # === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
 # === Examples
-#
-#  class { 'simple_motd':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
 # === Authors
 #
-# Author Name <author@domain.com>
+
 #
 # === Copyright
 #
 # Copyright 2017 Your name here, unless otherwise noted.
 #
-class simple_motd {
-
+class simple_motd (
+  $contenido = undef,
+  $plantilla = undef,
+){
+  if $contenido {
+    warning('Se brindo una plantilla y contenido, se ignorara el contenido del motd y sera remplazado.')
+    $contenido_del_motd = $contenido
+  } else {
+    $contenido_del_motd = template('motd/motd.erb')
+  }
+  
+  file {'/etc/motd':
+    ensure  => file,
+    backup  => false,
+    content => $contenido_del_motd,
+  }
 }
